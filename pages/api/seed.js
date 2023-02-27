7b47f8a391e5ab9c5eccafe3e16a0520b645de41
alimoinijazani <Ali_28848>
2023-02-27 16:16:30 +0330

    seed sample product

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,11 +1,14 @@
 import db from '../../utils/db';
 import data from './../../utils/data';
 import User from './../../Model/user';
+import Product from '../../Model/product';
 
 const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: 'seeded successfuly' });
 };
